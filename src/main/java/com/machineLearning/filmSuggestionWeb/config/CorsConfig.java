f6dfb44851f49f5efe586677979363af7ea73124
Van Ky <kydang26092004@gmail.com>
2024-11-15 20:07:42 +0700

    update

--- src/main/java/com/machineLearning/filmSuggestionWeb/config/CorsConfig.java
+++ src/main/java/com/machineLearning/filmSuggestionWeb/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://example.com"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allowed methods
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
